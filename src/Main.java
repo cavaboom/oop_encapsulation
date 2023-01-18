892998b2ce3cbe11243cb38b998f8a2e14ac233f
dendariev <den.dariev@gmail.com>
2023-01-19 02:06:57 +0800

    Готовое задание

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import transport.Car;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Car car1 = new Car("Lada", "Granta", 1.7, "yellow", 2015, "Russia", "Manual", "Sedan", "с865мк", 4, true);
+        Car.Key key1 = car1.new Key(true, true);
+        System.out.println(car1);
+        System.out.println();
+        int month = 11;
+        car1.changeTires(month);
+        System.out.println(car1);
+        System.out.println(key1);
     }
 }
